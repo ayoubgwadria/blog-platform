538eaf9d9b4d33158cedb03ba3b647bd7e32676d
ayoubgwadria <ayoubgwadria@gmail.com>
2025-02-20 00:21:38 +0100

    dependency issues resolved

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,42 +1,20 @@
-// src/app/app.module.ts
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { JwtModule } from '@auth0/angular-jwt';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-export function tokenGetter() {
-  return localStorage.getItem('access_token');
-}
-
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    
-    // Angular Material
-    MatSnackBarModule,
-
-    // JWT Configuration
-    JwtModule.forRoot({
-      config: {
-        tokenGetter,
-        allowedDomains: ['localhost:5000', 'api.yourdomain.com'],
-        disallowedRoutes: [
-          /\/auth\/login/,
-          /\/auth\/refresh/
-        ]
-      }
-    })
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
